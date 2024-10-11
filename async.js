8ff6674a599c973e2d8d5eddaddab4c1080eaa1c
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-10-11 23:13:14 +0600

    async js

---
+++ async.js
@@ -0,0 +1,29 @@
+const x = 'hello';
+console.log(x);
+console.log('pizza 1 started making');
+
+
+setTimeout(() =>{
+	console.log('pizza 1 done');
+}, 5000);
+
+console.log('pizza 2 started making');
+
+setTimeout(() =>{
+	console.log('pizza 2 done');
+}, 3000);
+
+console.log('pizza 3 started making');
+
+setTimeout(() =>{
+	console.log('pizza 3 done');
+}, 7000);
+
+
+
+
+
+// console.log(10);
+// if(true){
+// 	console.log('hello this is true');
+// }
