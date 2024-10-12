3b892595fcce87a87f727be937241f955b74d0b5
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-10-12 14:33:27 +0600

    fetch js

---
+++ fetch.js
@@ -0,0 +1,10 @@
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json))
+	  .catch(err => console.log(err))
+
+// const showData = async () =>{
+// 	const result = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+// 	const data = await result.json();
+// 	console.log(data);
+// }
