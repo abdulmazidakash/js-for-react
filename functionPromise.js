cc161073e40c7b68581c410e9e7227af37e752d4
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-10-12 15:54:18 +0600

    function promise complete

---
+++ functionPromise.js
@@ -0,0 +1,17 @@
+const getPromise = (name) =>{
+	return new Promise((resolve, reject) =>{
+		console.log(`Hello ${name}. your promise is made`);
+		setTimeout(() =>{
+			const error = false;
+			if(!error){
+				resolve(`${name} your promise is resolved`)
+			}else{
+				reject(`opps... ${name} error happend...`);
+			}
+		}, 3000)
+	})
+}
+
+getPromise('Akash')
+.then(res => console.log(res))
+.catch(err => console.log(err))
